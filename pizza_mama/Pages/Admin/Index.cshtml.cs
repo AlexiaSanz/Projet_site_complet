1a5aee774f34c237c1fa3795d2181080c9209100
alexia sanz <alexiasanzdegaldeano@gmail.com>
2022-03-17 11:29:34 +0100

    Ajoutez des fichiers projet.

---
+++ pizza_mama/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace pizza_mama.Pages.Admin
+{
+    
+    public class IndexModel : PageModel
+    {
+        public bool DisplayInvalidAccountMessage = false;
+
+        IConfiguration configuration;
+
+        public IndexModel(IConfiguration configuration)
+        {
+            this.configuration=configuration;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string username, string password, string ReturnUrl)
+        {
+            var authSection = configuration.GetSection("Auth");
+
+            string adminlogin = authSection["AdminLogin"];
+            string adminPassword = authSection["AdminPassword"];
+
+            if (username == adminlogin && password == adminPassword)
+            {
+                DisplayInvalidAccountMessage = false;
+
+                var claims = new List<Claim>
+                     {
+                     new Claim(ClaimTypes.Name, username)
+                     };
+                var claimsIdentity = new ClaimsIdentity(claims, "Login");
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new
+               ClaimsPrincipal(claimsIdentity));
+                return Redirect(ReturnUrl == null ? "/Admin/Pizzas" : ReturnUrl);
+            }
+
+            DisplayInvalidAccountMessage = true;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnGetLogout()
+        {
+           await HttpContext.SignOutAsync();
+            return Redirect("/admin");
+        }
+    }
+}
---
+++ pizza_mama/controllers/APIController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using pizza_mama.Models;
+using pizza_mama.Data;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace pizza_mama.controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class APIController : Controller
+    {
+        DataContext dataContext;
+       public APIController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+        
+        [HttpGet]
+        [Route("GetPizzas")]
+
+        public IActionResult GetPizzas()
+        {
+           var pizzas = dataContext.Pizzas.ToList();
+
+            return Json(pizzas);
+        }
+    }
+}
